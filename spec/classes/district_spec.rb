bf7b0f80791a88d20415b9b4d406cb6b90dfcf4e
atagunay <atafb2000gunay@gmail.com>
2022-12-26 22:16:17 +0300

    Add control for missing chars in turkis_alphabet list

---
+++ spec/classes/district_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/turkish_cities/district'
+
+RSpec.describe District do
+  describe '#list_neighborhoods' do
+    context 'when input is supported' do
+      it 'finds neighborhoods of given city and district' do
+        expect(District.new('Artvin', 'Yusufeli').neighborhoods(nil).count).to eq 674
+        expect(District.new('İstanbul', 'Adalar').neighborhoods(nil).count).to eq 5
+        expect(District.new('Ankara', 'Mamak').neighborhoods(nil).count).to eq 64
+      end
+    end
+  end
+end
